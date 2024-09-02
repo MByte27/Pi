046b17bb76ba325d3b330461fa480b3bf57c56e0
Pietro <105150784+MByte27@users.noreply.github.com>
2024-09-02 22:37:52 +0200

    Add files via upload

---
+++ brain.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ################## PRELIMINARY FUNCTION AND CODE ##################
+
+NTFY_SERVER="http://100.103.12.107:90"
+CHANNEL="ntPi3"
+
+# Send function
+send_ntfy() {
+  curl -H "Title: Update System" -H "Priority: $1" -d "$2" "$NTFY_SERVER/$CHANNEL"
+}
+# ################## ################## ##################
+
+# ################## UPDATES ##################
+
+# Delete old logs
+sudo rm -f /tmp/apt_upgrade.log
+sudo rm -f /tmp/apt_upgrade.log
+
+# Initialising update process: look for updates, low priority (No push)
+send_ntfy "2" "Scanning for updates .."
+# Store update log in temp file
+sudo apt update > /tmp/apt_update.log
+
+# Check upgradeables from temp log
+UPGRADE_COUNT=$(grep "packages can be upgraded." /tmp/apt_update.log | awk '{print $1}')
+
+# If UPGRADE_COUNT empty, set to 0
+UPGRADE_COUNT=${UPGRADE_COUNT:-0}
+
+if [ "$UPGRADE_COUNT" -gt 0 ]; then
+  # List upgradeable in notification
+  UPGRADE_LIST=$(apt list --upgradable 2>/dev/null | grep -v "Listing..." | awk '{print $1}')
+  send_ntfy "3" "Upgradeable:\n$UPGRADE_LIST"
+  
+  # Do full-upgrade and store to log
+  sudo apt full-upgrade -y > /tmp/apt_upgrade.log
+
+  # Send notification about how many pkgs have been updated. Priority increased (Default push)
+  send_ntfy "3" "Upgrade completed. $UPGRADE_COUNT upgraded packages."
+else
+  # Priority 2 (No notification)
+  send_ntfy "2" "Everything up to date."
+fi
