7708a737c826acb8fd56d6477dbcf7746cb9d0e3
Pietro <105150784+MByte27@users.noreply.github.com>
2024-09-02 22:15:39 +0200

    Update setup.sh
    Update and modification

--- setup.sh
+++ setup.sh
@@ -1,35 +1,4 @@
-#!/bin/bash
-
-# TESTED ON Raspberry Pi 0, Linux 6.6.31+rpt-rpi-v6
-
-src="/home/pietro/Raspberry/essentials"
-
-copy_in_system() {
-  # $1 = etc or home/pietro
-  # $2 = ntfy, uptime-kuma, agnostics or anything else
-  sudo cp -r "$src/$1/$2" "/$1"
-  echo "/$1/$2"
-}
-
-activate() {
-  cd "$1"
-  # Make sure you're in the right directory before executing this
-  if [ -f "docker-compose.yml" ]; then
-    sudo docker-compose up -d
-  fi
-}
-
-for dir in "$src/etc/"*; do
-  # get dir name only
-  base_dir=$(basename "$dir")
-  res=$(copy_in_system "etc" "$base_dir")
-  activate "$res"
-done
-
-
------------------------------------------------------------------------------------------------------
 # v2
-
 #!/bin/bash
 
 # preliminary update of all packages installed ####
@@ -41,8 +10,23 @@ sudo apt full-upgrade -y
 sudo apt install fio docker-compose lsof -y
 # #################################################
 
-systemplace(){}
+move(){
+  src="/home/pietro/Pi"
+  sudo cp -r "$src/$1/$2" "/$1"
+}
 
-activateapp(){}
+activateapp(){
+  cd "$1"
+    # Make sure you're in the right directory before executing this
+    if [ -f "docker-compose.yml" ]; then
+      sudo docker-compose up -d
+    fi
+}
 
+for dir in "$src/etc/*"; do
+  # get dir name only
+  base_dir=$(basename "$dir")
+  res=$(copy_in_system "etc" "$base_dir")
+  activate "$res"
+done
 
