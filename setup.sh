c59cf1bf78fe691971489c1ac6d70a278ecf3fd2
Pietro <105150784+MByte27@users.noreply.github.com>
2024-09-06 16:50:06 +0200

    Update setup.sh
    Improvements in code structure and revised algorithm

--- setup.sh
+++ setup.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-# preliminary update of all packages installed ####
+# ----------- preliminary update of all installed packages -----------
 sudo apt update
 sudo apt full-upgrade -y
-# #################################################
+# ----------- ----------- ----------- ----------- ----------- --------
 
-# install pkgs required ###########################
+# ----------- install required packages -----------
 sudo apt install fio docker-compose lsof python3-pip python3-venv -y
-# #################################################
+# ----------- ----------- ----------- ----------- -
 
 src="/home/pietro/Pi"
+is_Zero=false
 
-move(){
-  sudo cp -r "$src/$1/$2" "/$1"
-  echo "/$1/$2"
-}
+# ----------- Find out if model is Pi Zero -----------
+if grep -q "Raspberry Pi Zero" /proc/device-tree/model; then
+  echo "Device is Pi Zero. Skipping Uptime Kuma.." # Uptime Kuma is incompatible with v6 kernel
+  is_Zero=true
+fi
+# ----------- ----------- ----------- ----------- ----
 
-docker_app_launch(){
-  local dir_path="$1"
-  if [ -d "$dir_path" ]; then
-    cd "$dir_path" || exit
-    # Make sure you're in the right directory before executing this
-    if [ -f "docker-compose.yml" ]; then
-      sudo docker-compose up -d
+# Loop over folders in the source directory
+for folder in "$src"/*; do
+    if [ -d "$folder" ]; then
+        dest="$folder"
+
+        # Loop over items inside each folder
+        for item in "$folder"/*; do
+            # Check if the item is "uptime-kuma" and skip it if the device is Pi Zero
+            if [[ "$(basename "$item")" = "uptime-kuma" ]] && [ "$is_Zero" = true ]; then
+                continue
+            fi
+
+            # If the destination folder is "etc", use sudo to copy the items
+            if [ "$(basename "$folder")" = "etc" ]; then
+                sudo cp -r "$item" "$dest"
+            else
+                cp -r "$item" "$dest"
+            fi
+        done
     fi
-  else
-    echo "Directory $dir_path does not exist."
-  fi
-}
-
-for dir in "$src"/*; do
-  # get dir name only
-  base_dir=$(basename "$dir")
-  res=$(move "etc" "$base_dir")
-  docker_app_launch "$res"
 done
